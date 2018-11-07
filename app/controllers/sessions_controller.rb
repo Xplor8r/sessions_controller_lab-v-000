b008d0e13ad709847eed37b9d032079eb0849c40
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-07 23:15:31 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name] 
-      session[:name] = params[:name] 
+    if params[:name]
+      session[:name] = params[:name]
       redirect_to controller: 'sessions', action: 'new'
     else
       redirect_to controller: 'sessions', action: 'new'
